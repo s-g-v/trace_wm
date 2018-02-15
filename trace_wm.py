ea068956fa1d37e5b256b37da08b56582e7b912b
s-g-v <qwertyk@bk.ru>
2018-02-15 16:08:51 +0300

    reduce number of 'if' statements

--- trace_wm.py
+++ trace_wm.py
@@ -32,11 +32,10 @@ def _send_message(ttl, dst, port):
     sender.sendto(b'', (dst, port))
     try:
         addr = receiver.recvfrom(1024)[1]
-        end_time = time.time()
-        duration = round((end_time - start_time) * 1000, 2)
-        return addr, duration
+        duration = round((time.time() - start_time) * 1000, 2)
+        return '{:<4} {:<15}  {:<6}ms'.format(ttl, addr[0], duration), get_location(addr[0])
     except socket.timeout:
-        pass
+        return '{:<4} *  *  *'.format(ttl), None
     finally:
         receiver.close()
         sender.close()
@@ -47,8 +46,7 @@ def get_location(ip):
     location = []
     if result:
         location.append(result.country)
-        if result.subdivisions:
-            location.extend(result.subdivisions)
+        location.extend(result.subdivisions)
         if result.timezone != 'None':
             location.append(result.timezone)
         location.append(result.location)
@@ -61,35 +59,31 @@ def trace(dst, hops=30):
         dst_ip = socket.gethostbyname(dst)
     except socket.error as e:
         raise IOError('Unable to resolve {}:\n{}'.format(dst, e))
-    header = 'Trace to {}({}) in max {} hops'.format(dst, dst_ip, args.hops)
+    header = 'Trace to {}({}) in max {} hops'.format(dst, dst_ip, hops)
     world_map = WorldMap(*get_terminal_size())
-    world_map.insert_line(0, 0, header)
+    world_map.insert_msg(0, 0, header)
     print(world_map)
-    routes, addr, ttl = [], [''], 1
-    last_location = None
-    while ttl < hops + 1 and addr[0] != dst_ip:
+    routes, ttl = [], 1
+    previous_location = None
+    while ttl < hops + 1:
         memento_map = copy.deepcopy(world_map)
-        response = _send_message(ttl, dst, port)
-        if response:
-            addr, duration = response
-            route = '{:<4} {:<15}  {:<6}ms'.format(ttl, addr[0], duration)
-            location = get_location(addr[0])
-            if location:
-                route += '  {}'.format(', '.join(location[:-1]))
-                # route += ' ({:.3f}, {:.3f})'.format(*location[-1])
-                if location != last_location:
-                    world_map.add_point(location[-1], '{} {}'.format(ttl, location[0]))
-                    last_location = location
-        else:
-            route = '{:<4} *  *  *'.format(ttl)
+        route, location = _send_message(ttl, dst, port)
+        if location:
+            route += '  ' + ', '.join(location[:-1])
+            # route += ' ({:.3f}, {:.3f})'.format(*location[-1])  # add coordinates to debug
+            if location != previous_location:
+                world_map.add_point(location[-1], '{} {}'.format(ttl, location[0]))
+                previous_location = location
         routes.append(route)
         memento_map.add_text(routes)
         print(memento_map)
+        if route.split()[1] == dst_ip:
+            break
         ttl += 1
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser('Simple traceroute implementation')
+    parser = argparse.ArgumentParser('Simple trace route implementation')
     parser.add_argument('host', metavar='host', type=str, help='Destination host to trace route')
     parser.add_argument('--hops', '-n', type=int, help='Max number of hops', required=False, default=30)
     args = parser.parse_args()
